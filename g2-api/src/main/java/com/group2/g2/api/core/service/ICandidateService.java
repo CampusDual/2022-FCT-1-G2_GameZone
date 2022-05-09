a16ec03f2353a214b279d1a16dc04e021a55c00f
moitaben <72012152+moitaben@users.noreply.github.com>
2022-05-09 17:44:14 +0200

    changed archtype name

--- g2-api/src/main/java/com/group2/g2/api/core/service/ICandidateService.java
+++ g2-api/src/main/java/com/group2/g2/api/core/service/ICandidateService.java
@@ -9,7 +9,7 @@ import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 public interface ICandidateService {
 
     // CANDIDATE
-    public EntityResult candidateQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+    public EntityResult candidateQuerys(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
     public EntityResult candidateInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
     public EntityResult candidateUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
     public EntityResult candidateDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
