5d4ad9ad3aedbec3655964739f2e7d7ccee57c1a
Seudmon <104391455+Seudmon@users.noreply.github.com>
2022-05-11 16:56:11 +0200

    add login

--- g2-model/src/main/java/com/group2/g2/model/core/dao/UserDao.java
+++ g2-model/src/main/java/com/group2/g2/model/core/dao/UserDao.java
@@ -17,7 +17,6 @@ public class UserDao extends OntimizeJdbcDaoSupport {
 
 	public static final String USR_EMAIL    = "user_email";
 	public static final String USR_PASSWORD = "user_password";
-
 	public static final String ID            = "user_id";
 	public static final String EMAIL         = "user_email";
 	public static final String PASSWORD      = "user_password";
@@ -26,5 +25,6 @@ public class UserDao extends OntimizeJdbcDaoSupport {
 	public static final String SCHEMA        = "db_schema";
 	public static final String CREATION_DATE = "user_creation_date";
 	public static final String DOWN_DATE     = "user_down_date";
+	public static final String BIRTHDAY     = "user_birthday";
 
 }
