380de16b01b3acd6f629f67ed7b31b9c8914b395
Seudmon <104391455+Seudmon@users.noreply.github.com>
2022-05-18 14:05:20 +0200

    hOla

--- g2-model/src/main/java/com/group2/g2/model/core/service/Favorite_gamesService.java
+++ g2-model/src/main/java/com/group2/g2/model/core/service/Favorite_gamesService.java
@@ -1,5 +1,7 @@
 package com.group2.g2.model.core.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
 import com.group2.g2.api.core.service.IFavorite_gamesService;
 import com.group2.g2.model.core.dao.Favorite_gamesDao;
+import com.group2.g2.model.core.dao.GamesDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
@@ -20,6 +24,8 @@ public class Favorite_gamesService implements IFavorite_gamesService {
 	private Favorite_gamesDao favorite_gamesDao;
 	@Autowired
 	private DefaultOntimizeDaoHelper daoHelper;
+	@Autowired
+	private GamesService gamesService;
 
 	@Override
 	public EntityResult favorite_gamesQuery(Map<String, Object> keyMap, List<String> attrList)
@@ -29,6 +35,31 @@ public class Favorite_gamesService implements IFavorite_gamesService {
 
 	@Override
 	public EntityResult favorite_gamesInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+
+		Map<String, Object> gameKey = new HashMap<>();
+		gameKey.put(GamesDao.ATTR_GAME_NAME, attrMap.remove(GamesDao.ATTR_GAME_NAME));
+
+		List<String> game_id = new ArrayList<String>();
+		game_id.add(GamesDao.ATTR_ID);
+
+		EntityResult gamesQuery = gamesService.gamesQuery(gameKey, game_id);
+
+		if (gamesQuery.getCode() != EntityResult.OPERATION_WRONG) {
+			if (gamesQuery.calculateRecordNumber() > 0) {
+				Map recordValues = gamesQuery.getRecordValues(0);
+				attrMap.put(Favorite_gamesDao.ATTR_GAME_ID, recordValues.get(GamesDao.ATTR_ID));
+
+			} else {
+				EntityResult gamesInsert = gamesService.gamesInsert(gameKey);
+				if (gamesInsert.getCode() != EntityResult.OPERATION_WRONG) {
+					gamesInsert.get(GamesDao.ATTR_ID);
+					attrMap.put(Favorite_gamesDao.ATTR_GAME_ID, gamesInsert.get(GamesDao.ATTR_ID));
+
+				}
+			}
+
+		}
+
 		return this.daoHelper.insert(this.favorite_gamesDao, attrMap);
 	}
 
