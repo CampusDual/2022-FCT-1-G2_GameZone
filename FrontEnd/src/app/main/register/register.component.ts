4bc01681bdc3ff70ca82dbbe7c98f645138d91a5
moitaben <72012152+moitaben@users.noreply.github.com>
2022-05-20 10:27:30 +0200

    Add Register function

--- FrontEnd/src/app/main/register/register.component.ts
+++ FrontEnd/src/app/main/register/register.component.ts
@@ -1,16 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "ontimize-web-ngx";
+import {HttpClient} from "@angular/common/http";
+import {User} from "./user";
+import {Observable} from "rxjs";
+import {FormControl, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: RegisterComponent
+    }
+  ]
+
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(public authService : AuthService) { }
+
+  fc  = new FormControl()
+  user = new User();
+  auth = btoa("demo:demouser")
+
+  baseURL: string = "http://localhost:33333/users/user"
+
+  constructor(public authService : AuthService, private http : HttpClient) {
+    this.user.birthday = new Date()
+  }
 
   ngOnInit() {
   }
 
+  addUser() {
+    this.Register(this.user)
+      .subscribe(data => {
+        console.log(data)
+      })
+  }
+
+
+  Register(user:User): Observable<any> {
+    const headers = { 'content-type': 'application/json', 'Authorization' : 'Basic ' + this.auth}
+    const body= "{\"data\": "+JSON.stringify(user) +"  ,\"sqltypes\": {\"user_\": 12,\"birthday\": 91}} " ;
+    console.log(body)
+    console.log(headers)
+    return this.http.post(this.baseURL, body,{'headers':headers})
+  }
 }
