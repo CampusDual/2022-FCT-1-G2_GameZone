dee26fb20a2f7195a219cbc6ca612240f746e6db
Alex <alopezro85@gmail.com>
2022-05-25 11:43:43 +0200

    alex

--- FrontEnd/src/app/main/home/home-routing.module.ts
+++ FrontEnd/src/app/main/home/home-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 import { HomeComponent } from './home.component';
+
 
 const routes: Routes = [
   {
