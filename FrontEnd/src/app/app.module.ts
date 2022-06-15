23f089d32a66aaa828c69a4f1aa8092ab1133240
moitaben <72012152+moitaben@users.noreply.github.com>
2022-06-15 11:54:56 +0200

    add preRegister component and  fix login

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CONFIG } from './app.config';
+import { PreRegisterComponent } from './preRegister/preRegister.component';
 
 // Standard providers...
 // Defining custom providers (if needed)...
