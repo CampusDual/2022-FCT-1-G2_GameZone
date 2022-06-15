bee4fdb3adc6d64735314c18eb9fb9729b431c36
Seudmon <104391455+Seudmon@users.noreply.github.com>
2022-06-15 11:32:32 +0200

    nothing

--- FrontEndAdmin/src/app/shared/app.menu.config.ts
+++ FrontEndAdmin/src/app/shared/app.menu.config.ts
@@ -7,7 +7,7 @@ export const MENU_CONFIG: MenuRootItem[] = [
   {id: 'update-price' , name: 'UPDATE PRICES', icon: 'euro', route: '/main/update-price'},
   {id: 'tournaments' , name: 'TOURNAMENTS', icon: 'emoji_events', route: '/main/tournaments'},
   {id: 'events' , name: 'EVENTS', icon: 'stadium', route: '/main/events'},
-  {id: 'new-subs' , name: 'NEW SUBS', icon: 'group_add', route: '/main/new-subs'},
-  {id: 'subs-price' , name: 'SUBS AND PRICE', icon: 'people', route: '/main/subs-price'}
+  {id: 'new-subs' , name: 'NEW_SUBS', icon: 'group_add', route: '/main/new-subs'},
+  {id: 'subs-price' , name: 'SUBS_AND_INCOME', icon: 'people', route: '/main/subs-price'}
 ];
 
