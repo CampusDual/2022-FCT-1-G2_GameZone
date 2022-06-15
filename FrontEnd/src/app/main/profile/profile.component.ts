6a0583b694a0d96952aa989e3d38e6fe90c787ee
moitaben <72012152+moitaben@users.noreply.github.com>
2022-06-15 10:16:28 +0200

    add request to review component

--- FrontEnd/src/app/main/profile/profile.component.ts
+++ FrontEnd/src/app/main/profile/profile.component.ts
@@ -1,67 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
-
-export interface ProfileItems{
-
-  backgroundCover: string
+export interface ProfileItems {
+  backgroundCover: string;
   cover: {
-    id:number
-    url:string
-   }
-   coverUrl:string
-  title: string
-  dateRelease: string
-  genres:{
-    id:number
-    url:string
-  }
-  plataforms:{
-    id:number
-    url:string
-   }
-  description: string
-  memberRating: string
-  criticRating: string
-  videos: [{
-    id:number
-    video_id:string
-   }]
-  screenshots:[{
-    id:number
-    url:string
-   }]
-  artworks:[{
-    id:number
-    url:string
-   }]
+    id: number;
+    url: string;
+  };
+  coverUrl: string;
+  title: string;
+  dateRelease: string;
+  genres: {
+    id: number;
+    url: string;
+  };
+  plataforms: {
+    id: number;
+    url: string;
+  };
+  description: string;
+  memberRating: number;
+  criticRating: number;
+  videos: [
+    {
+      id: number;
+      video_id: string;
+    }
+  ];
+  screenshots: [
+    {
+      id: number;
+      url: string;
+    }
+  ];
+  artworks: [
+    {
+      id: number;
+      url: string;
+    }
+  ];
   recommendations: {
-    id:number
-    url:string
-   }
-  dataScreenshots : string[]
-  dataVideos : string[]
-  dataArtworks : string[]
-  id:number
-  name:string
+    id: number;
+    url: string;
+  };
+  dataScreenshots: string[];
+  dataVideos: string[];
+  dataArtworks: string[];
+  id: number;
+  aggregated_rating: number;
+  total_rating: number;
 }
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
-
 export class ProfileComponent implements OnInit {
   id: string;
 
-  constructor(private route : ActivatedRoute) {
-    this.id = this.route.snapshot.paramMap.get("id")
+  constructor(private route: ActivatedRoute) {
+    this.id = this.route.snapshot.paramMap.get("id");
   }
 
   ngOnInit() {
-
-    console.log(this.id)
-
+    console.log(this.id);
   }
 }
--- FrontEnd/src/app/main/profile/reviews/reviews.component.ts
+++ FrontEnd/src/app/main/profile/reviews/reviews.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ProfileItems } from "../profile.component";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-reviews',
-  templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.css']
+  selector: "app-reviews",
+  templateUrl: "./reviews.component.html",
+  styleUrls: ["./reviews.component.css"],
 })
-export class ReviewsComponent implements OnInit {
+export class ReviewsComponent implements AfterViewInit {
+  readonly SEARCH_URL = "http://localhost:33333/profile/profile?id=";
+  data: ProfileItems[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.http
+      .get<ProfileItems[]>(this.SEARCH_URL + 126459)
+      .pipe(
+        map((item) => {
+          return item.map((x) => {
+            return {
+              criticRating:  Math.trunc(x.aggregated_rating),
+              memberRating: Math.trunc(x.total_rating)
+            } as ProfileItems;
+          });
+        })
+      )
+      .subscribe(
+        (data) => (this.data = data),
+        (error) => console.log(error)
+      );
   }
-
 }
