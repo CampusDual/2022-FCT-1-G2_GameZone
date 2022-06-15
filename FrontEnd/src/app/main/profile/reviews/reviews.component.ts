aacd8b89fa93027e23d39f5896f6047c372cd628
moitaben <72012152+moitaben@users.noreply.github.com>
2022-06-15 11:11:52 +0200

    add review component html  and service

--- FrontEnd/src/app/main/profile/reviews/reviews.component.ts
+++ FrontEnd/src/app/main/profile/reviews/reviews.component.ts
@@ -1,35 +1,228 @@
-import { AfterViewInit, Component } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { ProfileItems } from "../profile.component";
 import { map } from "rxjs/operators";
+import {
+  ApexChart,
+  ApexFill,
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexStroke,
+  ChartComponent,
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+  stroke: ApexStroke;
+};
+
+export type ChartOptions2 = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+  stroke: ApexStroke;
+};
 
 @Component({
   selector: "app-reviews",
   templateUrl: "./reviews.component.html",
   styleUrls: ["./reviews.component.css"],
 })
-export class ReviewsComponent implements AfterViewInit {
+export class ReviewsComponent implements OnInit {
   readonly SEARCH_URL = "http://localhost:33333/profile/profile?id=";
   data: ProfileItems[];
-
-  constructor(private http: HttpClient) {}
-
-  ngAfterViewInit() {
+  dataLoaded : Promise<boolean>
+  // @ts-ignore
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+  public chartOptions2: Partial<ChartOptions>;
+  constructor(private http: HttpClient) {
     this.http
       .get<ProfileItems[]>(this.SEARCH_URL + 126459)
       .pipe(
         map((item) => {
           return item.map((x) => {
             return {
-              criticRating:  Math.trunc(x.aggregated_rating),
-              memberRating: Math.trunc(x.total_rating)
+              criticRating: Math.trunc(x.aggregated_rating),
+              memberRating: Math.trunc(x.total_rating),
             } as ProfileItems;
           });
         })
       )
       .subscribe(
         (data) => (this.data = data),
-        (error) => console.log(error)
+        (error) => console.log(error),
+        ()=> this.dataLoaded = Promise.resolve(true)
       );
+
+
+  }
+
+  ngOnInit() {
+    this.chartOptions = {
+      // @ts-ignore
+      series: [75],
+      chart: {
+        height: 200,
+        type: "radialBar",
+        toolbar: {
+          show: false
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -135,
+          endAngle: 225,
+          hollow: {
+            margin: 0,
+            size: "70%",
+            background: "#fff",
+            image: undefined,
+            position: "front",
+            dropShadow: {
+              enabled: true,
+              top: 3,
+              left: 0,
+              blur: 4,
+              opacity: 0.24
+            }
+          },
+          track: {
+            background: "#fff",
+            strokeWidth: "67%",
+            margin: 0, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: -3,
+              left: 0,
+              blur: 4,
+              opacity: 0.35
+            }
+          },
+
+          dataLabels: {
+            show: true,
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#888",
+              fontSize: "17px"
+            },
+            value: {
+              formatter: function(val) {
+                return parseInt(val.toString(), 10).toString();
+              },
+              color: "#111",
+              fontSize: "36px",
+              show: true
+            }
+          }
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "dark",
+          type: "horizontal",
+          shadeIntensity: 1,
+          gradientToColors: ["#ABE5A1"],
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100]
+        }
+      },
+      stroke: {
+        lineCap: "round"
+      },
+      labels: ["Critics Rating"]
+    };
+    this.chartOptions2 = {
+      // @ts-ignore
+      series: [75],
+      chart: {
+        height: 250,
+        type: "radialBar",
+        toolbar: {
+          show: false
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -135,
+          endAngle: 225,
+          hollow: {
+            margin: 0,
+            size: "70%",
+            background: "#fff",
+            image: undefined,
+            position: "front",
+            dropShadow: {
+              enabled: true,
+              top: 3,
+              left: 0,
+              blur: 4,
+              opacity: 0.24
+            }
+          },
+          track: {
+            background: "#fff",
+            strokeWidth: "67%",
+            margin: 0, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: -3,
+              left: 0,
+              blur: 4,
+              opacity: 0.35
+            }
+          },
+
+          dataLabels: {
+            show: true,
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#888",
+              fontSize: "17px"
+            },
+            value: {
+              formatter: function(val) {
+                return parseInt(val.toString(), 10).toString();
+              },
+              color: "#111",
+              fontSize: "36px",
+              show: true
+            }
+          }
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "dark",
+          type: "horizontal",
+          shadeIntensity: 1,
+          gradientToColors: ["#ABE5A1"],
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100]
+        }
+      },
+      stroke: {
+        lineCap: "round"
+      },
+      labels: ["Members Rating"]
+    };
   }
 }
+
+
+
--- FrontEnd/src/app/main/profile/reviews/reviews.module.ts
+++ FrontEnd/src/app/main/profile/reviews/reviews.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ReviewsRoutingModule } from './reviews-routing.module';
 import {ReviewsComponent} from "./reviews.component";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 
 @NgModule({
   declarations: [ReviewsComponent],
-  imports: [
-    CommonModule,
-    ReviewsRoutingModule
-  ]
+  imports: [CommonModule, ReviewsRoutingModule, NgApexchartsModule],
 })
-export class ReviewsModule { }
+export class ReviewsModule {}
