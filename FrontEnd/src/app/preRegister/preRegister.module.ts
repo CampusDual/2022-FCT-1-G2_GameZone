094f6ba2a532a8f6beca10c4b64032c76a5d19e6
moitaben <72012152+moitaben@users.noreply.github.com>
2022-06-15 12:35:59 +0200

    change  Css

--- FrontEnd/src/app/preRegister/preRegister.module.ts
+++ FrontEnd/src/app/preRegister/preRegister.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreRegisterRoutingModule } from './preRegister-routing.module';
 import {PreRegisterComponent} from "./preRegister.component";
+import {OntimizeWebModule} from "ontimize-web-ngx";
 
 
 @NgModule({
   declarations: [PreRegisterComponent],
-  imports: [
-    CommonModule,
-    PreRegisterRoutingModule
-  ]
+  imports: [CommonModule, PreRegisterRoutingModule, OntimizeWebModule],
 })
-export class PreRegisterModule { }
+export class PreRegisterModule {}
