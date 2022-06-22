4e0463dc845e73deb7c919d56ea4320fa9711d31
Marta Calo Vales <martaja9@gmail.com>
2022-06-22 10:10:20 +0200

    Added interface to tournaments and events component

--- FrontEnd/src/app/main/eventos/eventos.component.ts
+++ FrontEnd/src/app/main/eventos/eventos.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Timestamp } from "rxjs/internal/operators/timestamp";
+
+interface Event {
+  end_date: Timestamp<any>;
+  event_name: string;
+  description: string;
+  id: number;
+  start_date: number;
+  users:string []
+  }
+  interface User {
+  description: string
+  end_date: Timestamp<any>
+  event_id: number
+  event_name: string
+  start_date: Timestamp<any>
+  user_name: string
+  }
 
 @Component({
   selector: "app-eventos",
@@ -7,20 +25,39 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./eventos.component.css"],
 })
 export class EventosComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+  dataEvents: Event[];
+  dataUsers: User[];
 
-  ngOnInit() {
-    this.http
-      .get(
-        "http://localhost:33333/events/eventUsers?columns=event_id,user_name,event_name,description,start_date,end_date"
-      )
-      .subscribe((res) => console.log(res));
-
+  constructor(private http: HttpClient) {
     this.http
       .get(
         "http://localhost:33333/events/events?columns=id,event_name,description,start_date,end_date,"
       )
-      // @ts-ignore
-      .subscribe((res) => console.log(...res.data));
+
+      .subscribe(
+        // @ts-ignore
+        (res) => (this.dataEvents = [...res.data]),
+        (err) => console.log(err),
+        () => console.log("this.dataEvents", this.dataEvents)
+      );
+
+    this.http
+      .get(
+        "http://localhost:33333/events/eventUsers?columns=event_id,user_name,event_name,description,start_date,end_date"
+      )
+
+      .subscribe(
+        // @ts-ignore
+        (res) => (this.dataUsers = [...res.data]),
+        (err) => console.log(err),
+        () => this.dataEvents.forEach((x)=>{
+          x.users=[]
+          this.dataUsers.forEach((y)=>{
+          if(y.event_id===x.id)x.users.push(y.user_name)
+          })
+          })
+      );
   }
+
+  ngOnInit() {}
 }
--- FrontEnd/src/app/main/torneos/torneos.component.ts
+++ FrontEnd/src/app/main/torneos/torneos.component.ts
@@ -1,29 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Timestamp } from "rxjs/internal/operators/timestamp";
+
+interface Tournament {
+  id: number;
+  tour_name: string;
+  description: string;
+  start_date: Timestamp<any>;
+  end_date: Timestamp<any>;
+  users: string[];
+}
+
+ interface User {
+  tour_id: number;
+  user_name: string;
+  event_name: string;
+  description: string;
+  start_date: Timestamp<any>;
+  end_date: Timestamp<any>;
+}
 
 @Component({
-  selector: 'app-torneos',
-  templateUrl: './torneos.component.html',
-  styleUrls: ['./torneos.component.css']
+  selector: "app-torneos",
+  templateUrl: "./torneos.component.html",
+  styleUrls: ["./torneos.component.scss"],
 })
 export class TorneosComponent implements OnInit {
+  dataTournaments: Tournament[];
+  dataUsers: User[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http
       .get(
         "http://localhost:33333/tournaments/tourUsers?columns=tour_name,description,start_date,end_date,tour_id,user_name,tour_name"
       )
-      .subscribe((res) => console.log(res));
+      .subscribe(
+        // @ts-ignore
+        (res) => (this.dataUsers = [...res.data]),
+        (err) => console.log(err)
+      );
 
     this.http
       .get(
         "http://localhost:33333/tournaments/tournaments?columns=id,tour_name,description,start_date,end_date"
       )
       // @ts-ignore
-      .subscribe((res) => console.log(...res.data));
-  }
+      .subscribe((res) => (this.dataTournaments = [...res.data]),
+        (err) => console.log(err)),
+        this.dataTournaments.forEach((x) => {
+          x.users = [];
+          this.dataUsers.forEach((y) => {
+          if (y.tour_id === x.id) {
+          x.users.push(y.user_name);
+          }
+          });
+          })
   }
 
-
+}
