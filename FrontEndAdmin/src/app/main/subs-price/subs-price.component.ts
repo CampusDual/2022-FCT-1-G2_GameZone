32eeaeb681a7624e3c4b949b9e015e4c2842826d
Seudmon <104391455+Seudmon@users.noreply.github.com>
2022-06-27 11:27:58 +0200

    fix the table but it shows like 01/07/2022

--- FrontEndAdmin/src/app/main/subs-price/subs-price.component.ts
+++ FrontEndAdmin/src/app/main/subs-price/subs-price.component.ts
@@ -1,35 +1,122 @@
-import { Component, OnInit } from '@angular/core';
-import { MultiBarChartConfiguration} from 'ontimize-web-ngx-charts';
-import { D3LocaleService } from 'src/app/shared/d3-locale/d3Locale.service';
+import { Component, Injector, ViewChild } from '@angular/core';
+import { OntimizeService } from 'ontimize-web-ngx';
+import { ChartService, OChartComponent} from 'ontimize-web-ngx-charts';
 
+declare var d3: any;
 @Component({
   selector: 'app-subs-price',
   templateUrl: './subs-price.component.html',
   styleUrls: ['./subs-price.component.css']
 })
-export class SubsPriceComponent implements OnInit {
 
-  public movementTypesChartParams: MultiBarChartConfiguration;
+export class SubsPriceComponent{
+
+  @ViewChild('multiBar', {static: false})
+  protected multiBar: OChartComponent;
+
+  protected yAxis: string = 'MOVEMENT';
+  protected xAxis: string = 'DATE_';
+
+  protected data: Array<Object>;
+
+  constructor(protected injector: Injector) {  }
+
+  ngAfterViewInit() {
+
+    if (this.multiBar) {
+      let chartService: ChartService = this.multiBar.getChartService();
+      if (chartService) {
+        let chartOps = chartService.getChartOptions();
+
+        // Configuring x axis...
+        chartOps['xAxis']['tickFormat'] =
+          function (d) {
+            return d3.time.format('%m/%Y')(new Date(d));
+          };
+          chartOps['yAxis']['tickFormat'] = function (d) {
+            return d3.format(',f')(d) + '€';
+          };
 
 
-  constructor(
-    private d3LocaleService: D3LocaleService
-  ) {
-    const d3Locale = this.d3LocaleService.getD3LocaleConfiguration();
-    this._configureMultiBar(d3Locale);
-   }
+        // Configuring y axis...
+        var yScale = d3.scale.linear();
+        chartOps['yScale'] = yScale;
+        chartOps['yDomain'] = [-1000, 6000];
+      }
+    }
 
-  ngOnInit() {
+    // Ontimize service query...
+    let service: OntimizeService = this.injector.get(OntimizeService);
+    let conf = service.getDefaultServiceConfiguration();
+    conf['entity'] = 'EMovements';
+    service.configureService(conf);
+
+    let filter = {
+      'ACCOUNTID': 7310
+    }
+    service.query(filter, []).subscribe((resp) => {
+      if (resp.code === 0) {
+        this.adaptResult(resp.data);
+      }
+    });
   }
 
-  private _configureMultiBar(locale: any): void {
-    this.movementTypesChartParams = new MultiBarChartConfiguration();
-    this.movementTypesChartParams.margin.top = 0;
-    this.movementTypesChartParams.margin.right = 0;
-    this.movementTypesChartParams.margin.bottom = 0;
-    this.movementTypesChartParams.margin.left = 0;
+  adaptResult(data: Array<any>) {
+    if (data && data.length) {
+      let values = this.processValues(data);
+      this.data = [
+        {
+          'key': 'Movement',
+          'values': values['movement'],
+          'color': '#3498db'
+        },
+        {
+          'key': 'Average Balance',
+          'values': values['average'],
+          'color': '#e74c3c'
+        },
+        {
+          'key': 'Total Balance',
+          'values': values['total'],
+          'color': '#f9c922'
+        }
+      ];
+    }
+  }
 
-    this.movementTypesChartParams.legend.vers = 'furious';
+  processValues(data: Array<Object>): Object {
+    let values = {
+      'movement': [],
+      'average': [],
+      'total': []
+    };
+    var self = this;
+    let balance = 0.0;
+    let average = 0.0;
+    data.forEach((item: any, index: number) => {
+      let val = {
+        'x': item[self.xAxis],
+        'y': item[self.yAxis]
+      };
+
+      balance += val.y;
+      let val2 = {
+        'x': val.x,
+        'y': balance
+      };
+
+      average += balance;
+      let val3 = {
+        'x': val.x,
+        'y': (average / (index + 1))
+      };
+
+      values['movement'].push(val);
+      values['average'].push(val3);
+      values['total'].push(val2);
+    });
+    return values;
   }
 
 }
+
