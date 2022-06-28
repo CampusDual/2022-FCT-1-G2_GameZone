dcaf2d3fedc865513aeda9147512c863cd966812
Seudmon <104391455+Seudmon@users.noreply.github.com>
2022-06-28 15:22:43 +0200

    partially fix the chart's bug

--- FrontEndAdmin/src/app/main/subs-price/subs-price.component.ts
+++ FrontEndAdmin/src/app/main/subs-price/subs-price.component.ts
@@ -1,6 +1,6 @@
-import { Component, Injector, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, Injector, ViewChild } from '@angular/core';
 import { OntimizeService } from 'ontimize-web-ngx';
-import { ChartService, OChartComponent} from 'ontimize-web-ngx-charts';
+import { ChartDataAdapter, ChartService, OChartComponent} from 'ontimize-web-ngx-charts';
 
 declare var d3: any;
 @Component({
@@ -13,16 +13,55 @@ export class SubsPriceComponent{
 
   @ViewChild('multiBar', {static: false})
   protected multiBar: OChartComponent;
+  @ViewChild('multiBar2', {static: false})
+  protected multiBar2: OChartComponent;
 
-  protected yAxis: string = 'MOVEMENT';
-  protected xAxis: string = 'DATE_';
-
-  protected data: Array<Object>;
-
-  constructor(protected injector: Injector) {  }
+  nationalBranches: number;
+  internationalBranches: number;
+  prueba: any;
+  prueba2: any;
+  constructor(
+    private ontimizeService: OntimizeService,
+    private cd: ChangeDetectorRef,
+    protected injector: Injector
+  ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('users'));
+    const self = this;
+    /* this.ontimizeService.query(void 0, ['user_'], 'subsPrice').subscribe(
+      res => {
+        if (res && res.data) {
+          const factory = this.multiBar.getChartDataAdapterFactory();
+          const adapter: ChartDataAdapter = factory.getAdapter('multiBar');
+          const adaptedResult = adapter.adaptResult(res.data);
+          console.log(self.prueba);
+        }
+      },
+      err => console.log(err),
+      () => this.cd.detectChanges()
+    ); */
+  }
 
   ngAfterViewInit() {
-
+    this.formatdata();
+    this.ontimizeService.query(void 0, ['user_'], 'subsPrice').subscribe(
+      res => {
+        if (res && res.data) {
+          const factory = this.multiBar.getChartDataAdapterFactory();
+          const adapter: ChartDataAdapter = factory.getAdapter('multiBar');
+          const adaptedResult = adapter.adaptResult(res.data);
+          this.prueba=adaptedResult;
+          const factory2 = this.multiBar2.getChartDataAdapterFactory();
+          const adapter2: ChartDataAdapter = factory2.getAdapter('multiBar');
+          const adaptedResult2 = adapter2.adaptResult(res.data);
+          this.prueba2=adaptedResult2;
+          console.log(this.prueba);
+        }
+      },
+      err => console.log(err),
+      () => this.cd.detectChanges()
+    );
+  }
+  formatdata(){
     if (this.multiBar) {
       let chartService: ChartService = this.multiBar.getChartService();
       if (chartService) {
@@ -36,87 +75,44 @@ export class SubsPriceComponent{
           chartOps['yAxis']['tickFormat'] = function (d) {
             return d3.format(',f')(d) + '€';
           };
-
-
-        // Configuring y axis...
-        var yScale = d3.scale.linear();
-        chartOps['yScale'] = yScale;
-        chartOps['yDomain'] = [-1000, 6000];
       }
     }
 
-    // Ontimize service query...
-    let service: OntimizeService = this.injector.get(OntimizeService);
-    let conf = service.getDefaultServiceConfiguration();
-    conf['entity'] = 'EMovements';
-    service.configureService(conf);
+    if (this.multiBar2) {
+      let chartService: ChartService = this.multiBar2.getChartService();
+      if (chartService) {
+        let chartOps = chartService.getChartOptions();
 
-    let filter = {
-      'ACCOUNTID': 7310
-    }
-    service.query(filter, []).subscribe((resp) => {
-      if (resp.code === 0) {
-        this.adaptResult(resp.data);
+        // Configuring x axis...
+        chartOps['xAxis']['tickFormat'] =
+          function (d) {
+            return d3.time.format('%m/%Y')(new Date(d));
+          };
+          chartOps['yAxis']['tickFormat'] = function (d) {
+            return d3.format(',f')(d) + '€';
+          };
       }
-    });
-  }
-
-  adaptResult(data: Array<any>) {
-    if (data && data.length) {
-      let values = this.processValues(data);
-      this.data = [
-        {
-          'key': 'Movement',
-          'values': values['movement'],
-          'color': '#3498db'
-        },
-        {
-          'key': 'Average Balance',
-          'values': values['average'],
-          'color': '#e74c3c'
-        },
-        {
-          'key': 'Total Balance',
-          'values': values['total'],
-          'color': '#f9c922'
-        }
-      ];
     }
   }
-
-  processValues(data: Array<Object>): Object {
-    let values = {
-      'movement': [],
-      'average': [],
-      'total': []
-    };
-    var self = this;
-    let balance = 0.0;
-    let average = 0.0;
-    data.forEach((item: any, index: number) => {
-      let val = {
-        'x': item[self.xAxis],
-        'y': item[self.yAxis]
-      };
-
-      balance += val.y;
-      let val2 = {
-        'x': val.x,
-        'y': balance
-      };
-
-      average += balance;
-      let val3 = {
-        'x': val.x,
-        'y': (average / (index + 1))
-      };
-
-      values['movement'].push(val);
-      values['average'].push(val3);
-      values['total'].push(val2);
-    });
-    return values;
-  }
-
 }
-
+/* export class BranchesCardComponent {
+  nationalBranches: number;
+  internationalBranches: number;
+  constructor(
+    private ontimizeService: OntimizeService,
+    private cd: ChangeDetectorRef
+  ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('users'));
+    const self = this;
+    this.ontimizeService.query(void 0, ['COLUMNAS'], 'subsPrice').subscribe(
+      res => {
+        if (res && res.data) {
+          self.nationalBranches = res.data.filter((e: any) => e['COUNTRY'] === 'Spain').length;
+          self.internationalBranches = res.data.length - self.nationalBranches;
+        }
+      },
+      err => console.log(err),
+      () => this.cd.detectChanges()
+    );
+  }
+} */
